32d635b6ee01335096f0622cfabd8c869d43741b
Fabricio Pirini <fabricio.pirini@gmail.com>
2020-02-16 23:21:59 +0100

    Implement player1 movement by keyboard arrows

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,139 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from 'react';
 import styled from 'styled-components';
 import { isEmpty } from 'lodash';
 
 const Canvas = styled.canvas`
-  border: 1px solid #CCC;
+  border: 10px solid #CCC;
+  image-rendering: pixelated;
+  image-rendering: crisp-edges;
+  width: 400px;
+  height: 400px;
 `;
 
 const App = () => {
   const [players, setPlayers] = useState({});
-  // const [fruits, setfruits] = useState({});
+  const [fruits, setFruits] = useState({});
   const screenEl = useRef(null);
+  const gridSize = 10;
 
-  const demoInfo = {
+  const demoPlayers = {
     player1: {
       color: 'red',
-      positionX: 0,
-      positionY: 0,
-      width: 50,
-      height: 50,
+      positionX: 1,
+      positionY: 1,
     },
     player2: {
-      color: 'green',
-      positionX: 100,
-      positionY: 150,
-      width: 50,
-      height: 50,
+      color: 'blue',
+      positionX: 2,
+      positionY: 3,
     },
   };
 
+  const demoFruits = {
+    fruit1: {
+      color: 'yellow',
+      positionX: 5,
+      positionY: 5,
+    },
+    fruit2: {
+      color: 'yellow',
+      positionX: 2,
+      positionY: 7,
+    },
+  };
+
+  const updatePlayer = (playerId, key, value) => {
+    setPlayers({
+      ...players,
+      [playerId]: {
+        ...players[playerId],
+        [key]: value,
+      },
+    });
+  };
+
+  const validPosition = (key, add) => (
+    players.player1[key] + add < gridSize && players.player1[key] + add >= 0
+      ? players.player1[key] + add
+      : players.player1[key]);
+
+  const movePlayer = {
+    ArrowRight(player) {
+      const newPos = validPosition('positionX', 1);
+      updatePlayer(player, 'positionX', newPos);
+    },
+    ArrowLeft(player) {
+      const newPos = validPosition('positionX', -1);
+      updatePlayer(player, 'positionX', newPos);
+    },
+    ArrowUp(player) {
+      const newPos = validPosition('positionY', -1);
+      updatePlayer(player, 'positionY', newPos);
+    },
+    ArrowDown(player) {
+      const newPos = validPosition('positionY', 1);
+      updatePlayer(player, 'positionY', newPos);
+    },
+  };
+
+  const handleKeyPress = useCallback(
+    (event) => {
+      const keyName = event.key;
+
+      if (!isEmpty(players)) {
+        movePlayer[keyName]('player1');
+      }
+    },
+    [players, movePlayer],
+  );
+
   useEffect(() => {
-    const setInitialPlayers = () => {
-      if (JSON.stringify(players) !== JSON.stringify(demoInfo)) {
-        setPlayers(demoInfo);
+    const initializeData = () => {
+      if (isEmpty(players)) {
+        setPlayers(demoPlayers);
+      }
+      if (isEmpty(fruits)) {
+        setFruits(demoFruits);
       }
     };
 
-    const drawPlayers = () => {
+    const renderScreen = () => {
       const context = screenEl.current.getContext('2d');
+      context.fillStyle = 'white';
+      context.clearRect(0, 0, 10, 10);
 
       if (!isEmpty(players)) {
-        Object.values(players).forEach((player) => {
+        Object.entries(players).forEach(([playerId, player]) => {
           context.fillStyle = player.color;
-          context.fillRect(player.positionX, player.positionY, player.width, player.height);
+          context.fillRect(player.positionX, player.positionY, 1, 1);
+        });
+      }
+
+      if (!isEmpty(fruits)) {
+        Object.entries(fruits).forEach(([fruitId, fruit]) => {
+          context.fillStyle = fruit.color;
+          context.fillRect(fruit.positionX, fruit.positionY, 1, 1);
         });
       }
     };
 
-    setInitialPlayers();
-    drawPlayers();
-  }, [players, demoInfo]);
+    initializeData();
+    renderScreen();
+
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [players, fruits, demoPlayers, demoFruits, handleKeyPress]);
 
   return (
     <>
-      <Canvas id="screen" ref={screenEl} width="500" height="500" />
+      <Canvas id="screen" ref={screenEl} width={gridSize} height={gridSize} />
     </>
   );
 };
